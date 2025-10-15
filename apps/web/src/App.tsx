4b9fdcedc17474b70f2eaf5574426f51d0a780d8
slicenfer <16222938+slicenfer@user.noreply.gitee.com>
2025-10-15 14:11:01 +0800

    fix: 添加 Ant Design App 组件以支持 message 和 Modal 静态方法
    - 在 ConfigProvider 中添加 App 组件包裹
    - 修复 message.error/info/success 无法显示的问题
    - 修复 Modal.confirm 无法弹出的问题
    - 确保所有 Ant Design 静态方法正常工作

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { ConfigProvider } from 'antd'
+import { ConfigProvider, App as AntApp } from 'antd'
 import zhCN from 'antd/locale/zh_CN'
 import { HomePage } from './pages/HomePage'
 import { ReportDetailPage } from './pages/ReportDetailPage'
@@ -13,31 +13,33 @@ import { ProtectedRoute } from './components/ProtectedRoute'
 function App() {
   return (
     <ConfigProvider locale={zhCN}>
-      <BrowserRouter>
-        <Routes>
-          {/* 公众页面 */}
-          <Route path="/" element={<HomePage />} />
-          <Route path="/reports/:reportNumber" element={<ReportDetailPage />} />
+      <AntApp>
+        <BrowserRouter>
+          <Routes>
+            {/* 公众页面 */}
+            <Route path="/" element={<HomePage />} />
+            <Route path="/reports/:reportNumber" element={<ReportDetailPage />} />
 
-          {/* 管理后台 */}
-          <Route path="/admin/login" element={<LoginPage />} />
-          <Route
-            path="/admin"
-            element={
-              <ProtectedRoute>
-                <AdminLayout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<DashboardPage />} />
-            <Route path="reports" element={<ReportsListPage />} />
-            <Route path="upload" element={<UploadPage />} />
-          </Route>
+            {/* 管理后台 */}
+            <Route path="/admin/login" element={<LoginPage />} />
+            <Route
+              path="/admin"
+              element={
+                <ProtectedRoute>
+                  <AdminLayout />
+                </ProtectedRoute>
+              }
+            >
+              <Route index element={<DashboardPage />} />
+              <Route path="reports" element={<ReportsListPage />} />
+              <Route path="upload" element={<UploadPage />} />
+            </Route>
 
-          {/* 404 */}
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </BrowserRouter>
+            {/* 404 */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </AntApp>
     </ConfigProvider>
   )
 }
